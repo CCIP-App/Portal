986028b0bad37a0111807fb4d9d5a05e7bdfc626
Denny Huang <denny0223@gmail.com>
2019-03-01 02:37:00 +0800

    Move build result to dist

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-for i in `find . ! -name 'index.json' ! -name 'example.json' -name '*.json' `; do jq -c --arg FILENAME "`basename $i`" '{path: ("https://portal.opass.app/" + $FILENAME), display_name, logo_url}' $i; done | jq -r -c -s . > index.json
+mkdir -p dist/events/
+
+for i in *.json; do
+	jq -c --arg FILENAME "`basename $i`" '{path: ("https://portal.opass.app/events/" + $FILENAME), display_name, logo_url}' $i
+	cp $i dist/events/
+done | jq -r -c -s . > dist/index.json
+
+cp web/* dist/
