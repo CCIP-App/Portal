82752c33239316254f6e00e102fd2b70d49ce76e
Denny Huang <denny0223@gmail.com>
2019-03-01 03:29:02 +0800

    For test

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ mkdir -p dist/events/
 for i in *.json; do
 	jq -c --arg FILENAME "`basename $i`" '{path: ("https://portal.opass.app/events/" + $FILENAME), display_name, logo_url}' $i
 	cp $i dist/events/
+
+	mkdir dist/`basename $i .json`
+	cp $i $_/index.json
 done | jq -r -c -s . > dist/index.json
 
 cp web/* dist/
