227b598cdec4073616440b5936fa6deb4947e513
Denny Huang <denny0223@gmail.com>
2019-06-30 04:00:32 +0800

    Add event_schema.js

---
+++ event_schema.js
@@ -0,0 +1,35 @@
+{
+  "event_id": "",
+  "display_name": {
+    "en": "",
+    "zh": "",
+    "<Any ISO 639-1 lang>": ""
+  },
+  "logo_url": "https://", // only accept HTTPS URL
+  "publish": { // only accept ISO 8601
+    "start": "2018-11-24T07:01:40+00:00",
+    "end": "2018-11-24T07:01:40+00:00"
+  },
+  "server_base_url": "https://<domain>",
+  "schedule_url": "https://<domain>/<file_path>.json",
+  "features": { // option
+    "irc": "https://",
+    "telegram": "https://",
+    "staffs": "https://",
+    "venue": "https://",
+    "sponsors": "https://",
+    "partners": "https://"
+  },
+  "custom_features": [
+    {
+      "icon": "https://", // only accept @3x.png max: 128x128
+      "display_name": {
+        "zh": "",
+        "en": "",
+        "<Any ISO 639-1 lang>": ""
+      },
+      "url": "https://"
+    },
+    ...
+  ]
+}
