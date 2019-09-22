88d02a64611de095759d4243d3301058deec41b5
Denny Huang <denny0223@gmail.com>
2019-09-22 14:47:24 +0800

    Update schema for 2020

--- event_schema.js
+++ event_schema.js
@@ -6,22 +6,35 @@
     "<Any ISO 639-1 lang>": ""
   },
   "logo_url": "https://", // only accept HTTPS URL
+  "event_date": { // only accept ISO 8601
+    "start": "2020-03-28T00:00:00+08:00",
+    "end": "2020-03-28T00:00:00+08:00"
+  }
   "publish": { // only accept ISO 8601
-    "start": "2018-11-24T07:01:40+00:00",
-    "end": "2018-11-24T07:01:40+00:00"
+    "start": "2020-02-23T00:00:00+08:00",
+    "end": "2020-08-31T00:00:00+08:00"
   },
-  "server_base_url": "https://<domain>",
-  "schedule_url": "https://<domain>/<file_path>.json",
   "features": [ // app follow the sequence
     {
-      "feature": "", // fastpass, schedule, announcement, puzzle, ticket, telegram, im, sponsors, staffs, venue, webview
+      "feature": "", // fastpass, schedule, announcement, puzzle, ticket, telegram, im, sponsors, staffs, venue, webview, wifi
       "icon": "https://" // require when feature is webview, only accept @3x.png max: 128x128
       "display_text": {
         "en": "",
         "zh": "",
         "<Any ISO 639-1 lang>": ""
       },
-      "url": "https://", // support placeholder {role}, {public_token}
+      "url": "https://",
+      /*
+       * only accept HTTPS URL, support placeholder {role}, {public_token}
+       * require when feature is fastpass, announcement or ticket, the url is server base url, https://<domain>
+       */
+      "wifi": [ // require when feature is wifi
+        {
+          "SSID": "SITCON 2020",
+          "password": ""
+        },
+        ...
+      ]
       "visible_roles": [ // option, default for all user, string need match server config
         "staff",
         "speaker"
