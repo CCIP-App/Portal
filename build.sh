90e283f5804c70a1ed892d8317d8bbfcf89cc3e2
腹黒い茶 <haraguroicha@tipsy.coffee>
2020-09-14 21:50:29 +0800

    fix for improve code factor

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 mkdir -p dist/events/
 
-while IFS=$'\t' read timestamp file; do
+while IFS=$'\t' read -r timestamp file; do
 	jq -c '{event_id, display_name, logo_url}' $file
 
-	mkdir dist/events/`basename $file .json`
+	mkdir dist/events/$(basename $file .json)
 	cp $file $_/index.json
 done < <(TZ=/usr/share/zoneinfo/UTC jq -r '[.event_date.start | sub("(?<before>.*):"; .before ) | strptime("%Y-%m-%dT%H:%M:%S%z") | todate | fromdate, input_filename] | @tsv' events/*.json | sort -nr) | jq -r -c -s . > dist/events/index.json
 
